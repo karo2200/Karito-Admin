5a6a9ef59cf641d512a1d916bb2979ec2a6d1629
reyhaneh.ramezani <reyhane_ramezanian@hotmail.com>
2025-11-25 13:38:28 +0330

    prd

--- config/index.js
+++ config/index.js
@@ -1,14 +1,14 @@
-const API_URL = 'https://dev-api8081.karito.net/graphql';
-const SUBSCRIPTION_URL = 'http://dev-api8081.karito.net/graphql';
-const BLOB_BASE_URL = 'http://dev-api8081.karito.net/graphql';
+const API_URL = 'https://api.karito.net/graphql';
+const SUBSCRIPTION_URL = 'https://api.karito.net/graphql';
+const BLOB_BASE_URL = 'https://api.karito.net/graphql';
 const BLOB_URL = '';
+
 const config = {
 	API_URL,
 	BLOB_URL,
 	BLOB_BASE_URL,
 	SUBSCRIPTION_URL,
 	//
-
 	//
 	FIREBASE: {
 		APP_ID: '',
