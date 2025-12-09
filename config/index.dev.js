6549a812016107bfbbb7074b528d420ccf3e5cdf
reyhaneh.ramezani <reyhane_ramezanian@hotmail.com>
2025-12-09 17:08:29 +0330

    bug

--- config/index.dev.js
+++ config/index.dev.js
@@ -1,8 +1,7 @@
-const API_URL = 'https://api.karito.net/graphql';
-const SUBSCRIPTION_URL = 'https://api.karito.net/graphql';
-const BLOB_BASE_URL = 'https://api.karito.net/graphql';
+const API_URL = 'https://dev-api8081.karito.net/graphql';
+const SUBSCRIPTION_URL = 'https://dev-api8081.karito.net/graphql';
+const BLOB_BASE_URL = 'https://dev-api8081.karito.net/graphql';
 const BLOB_URL = '';
-
 const config = {
 	API_URL,
 	BLOB_URL,
